1762d5348101cf0d11d9820b7094ac5dc44eaba9
Ziad Emad <ziaemad0111@gmail.com>
2024-10-18 22:32:41 +0300

    Create Register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import LoginForm from './components/Login/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Register from './component/register/Register';
 
 const App = () => {
     return (
         <>
             <LoginForm />
+            {/* <Register /> */}
         </>
     )
 }
