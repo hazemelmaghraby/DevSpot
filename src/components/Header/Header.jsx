7a92aa4676a60ab51801d9e40a6db0524b865780
Hazem Mohamed Elmaghraby <hazemelmaghraby.work@gmail.com>
2024-11-01 12:14:50 +0200

    avatars Developing pt.1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import './Header.css';
-
+import avatar1 from './../../img/PersonIcon1.png';
+import avatar2 from './../../img/PersonIcon2.png';
+import avatar3 from './../../img/PersonIcon2.png';
 const Header = () => {
     return (
         <section className='header'>
             <div className='header-container'>
                 <div className='avatarContainer'>
                     {/* Example avatars, you can add more as needed */}
-                    <img src="avatar1.png" alt="Avatar 1" className="avatar avatar1" />
-                    <img src="avatar2.png" alt="Avatar 2" className="avatar avatar2" />
-                    <img src="avatar3.png" alt="Avatar 3" className="avatar avatar3" />
+                    <img src={avatar1} alt="Avatar 1" className="avatar avatar1" />
+                    <img src={avatar2} alt="Avatar 2" className="avatar avatar2" />
+                    <img src={avatar3} alt="Avatar 3" className="avatar avatar3" />
+                    <img src={avatar1} alt="Avatar 4" className="avatar avatar4" />
+                    <img src={avatar2} alt="Avatar 5" className="avatar avatar5" />
+                    <img src={avatar3} alt="Avatar 6" className="avatar avatar6" />
+                    <img src={avatar1} alt="Avatar 7" className="avatar avatar7" />
+                    <img src={avatar2} alt="Avatar 8" className="avatar avatar8" />
                 </div>
                 <div className='mainOfHomePageContainer'>
                     <div className='titleContainer'>
